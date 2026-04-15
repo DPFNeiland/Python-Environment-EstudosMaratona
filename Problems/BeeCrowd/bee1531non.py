ebab204fba51c267e293a46d3223b143d98dd520
Neiland <rodrigo.naohan@gmail.com>
2026-04-15 08:31:56 -0300

    Update bee1531non.py

--- Problems/BeeCrowd/bee1531non.py
+++ Problems/BeeCrowd/bee1531non.py
@@ -1,19 +1,17 @@
-def fib_mod(n, m):
-    # Encontra período de Pisano
-    pisano = [0, 1]
-    for i in range(2, m * m + 1):
-        pisano.append((pisano[-1] + pisano[-2]) % m)
-        if pisano[-2] == 0 and pisano[-1] == 1:
-            pisano.pop()
-            pisano.pop()
-            break
-    p = len(pisano)
-    return pisano[n % p]
+from math import pow
 
+def fib(n, m):
+    
+    inicial = [
+                [1,1],
+                [1,0]
+            ]
+    
+    for i in range(n):
+        inicial[0]                
+    
+    return inicial[0][1]
+    
 while True:
-    try:
-        n, m = map(int, input().split())
-        fn = fib_mod(n, 2_000_000_000)  # só para exemplo, ajustar aqui
-        print(fib_mod(fn, m))
-    except EOFError:
-        break
+    n, m  = map(int, (input().split()))
+    print(fib(fib(n, m),m))
